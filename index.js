8b091a157dbfed8633fd41c85ca916b18f0af43c
Julian Johannesen <julian.johannesen@gmail.com>
2018-02-16 00:30:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your code in this file!
+  function scuberGreetingForFeet(feet){
+    if(feet <= 400){
+      return "This one is on me!";
+    } else if(feet > 400 && feet <= 2000){
+      return "I will gladly take your thirty bucks.";
+    } else if(feet > 2500){
+      return "No can do.";
+    }
+  }
+  function ternaryCheckCity(city){
+    city === "NYC" ? "Ok, sounds good." : "No go.";
+  }
+  function switchOnCharmFromTip(tip){
+    switch (tip === "generous") {
+      case true:
+        return "Thank you so much.";
+      case false:
+        return "Thank you.";
+      default:
+        return "Bye.";
+    }
+  }
+console.log(scuberGreetingForFeet(500));
+console.log(ternaryCheckCity("NYC"));
+console.log(switchOnCharmFromTip("generous"));
