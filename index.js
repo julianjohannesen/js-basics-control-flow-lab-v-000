d87f4268b0c49d2acd0994123bcf27ec264674a1
Julian Johannesen <julian.johannesen@gmail.com>
2018-02-16 00:55:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
     }
   }
   function ternaryCheckCity(city){
-    city === "NYC" ? "Ok, sounds good." : "No go.";
+    city === "NYC" ? return "Ok, sounds good." : return "No go.";
   }
   function switchOnCharmFromTip(tip){
     switch (tip) {
